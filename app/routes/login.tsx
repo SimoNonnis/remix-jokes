626122cf0662de5993336b799beaca7e95837819
Simon Nonnis <simon.nonnis@protonmail.com>
2023-04-05 18:24:53 +0300

    Build login route and form

---
+++ app/routes/login.tsx
@@ -0,0 +1,71 @@
+import type { LinksFunction } from "@remix-run/node";
+import { useSearchParams, Link } from "@remix-run/react";
+import styleUrl from "~/styles/login.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: styleUrl }];
+};
+
+export default function Login() {
+  const [searchParams] = useSearchParams();
+
+  return (
+    <div className="container">
+      <div className="content" data-light="">
+        <h1>Login</h1>
+
+        <form method="post">
+          <input
+            type="hidden"
+            name="redirectTo"
+            value={searchParams.get("redirectTo") ?? undefined}
+          />
+
+          <fieldset>
+            <legend className="sr-only">Login or Register?</legend>
+
+            <label>
+              <input
+                type="radio"
+                name="loginType"
+                value="login"
+                defaultChecked
+              />
+              Login
+            </label>
+
+            <label>
+              <input type="radio" name="loginType" value="register" />
+              Register
+            </label>
+          </fieldset>
+
+          <div>
+            <label htmlFor="username-input">Username</label>
+            <input type="text" name="username" id="username-input" />
+          </div>
+
+          <div>
+            <label htmlFor="password-input">Password</label>
+            <input type="password" name="password" id="password-input" />
+          </div>
+
+          <button type="submit" className="button">
+            Submit
+          </button>
+        </form>
+
+        <div className="links">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/jokes">Jokes</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
